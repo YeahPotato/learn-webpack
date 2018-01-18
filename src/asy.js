fcd98894ab534a8b8300921b299254a59322dafa
yangwd <yangwd@gooalgene.com>
2018-01-18 16:23:20 +0800

    done

--- src/asy.js
+++ src/asy.js
@@ -1,14 +1,14 @@
 let as = async ()=>{
     await new Promise((resolve,reject)=>{
         setTimeout(()=>{
-            alert(1);
+            console.log(1);
             resolve();
         },1000)
     })
 
     await new Promise((resolve,reject)=>{
         setTimeout(()=>{
-            alert(2);
+            console.log(2);
             resolve();
         },200)
     })
--- src/dom.js
+++ src/dom.js
@@ -1,6 +1,9 @@
+import './style/index.css';
+
+
 let user = document.createElement('img');
 user.src = require('./images/user.jpg');
-user.style.width = '100px';
+user.style.width = '150px';
 document.body.appendChild(user);
 
 export default user;
